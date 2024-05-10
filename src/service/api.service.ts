59f0c951e4c23cba0bd56cbb5a5dda4652a4e088
ShaedulOffDev <shaeduloffdev@gmail.com>
2024-05-10 21:41:34 +0500

    refactor: change api url

--- src/service/api.service.ts
+++ src/service/api.service.ts
@@ -1,21 +1,23 @@
+const BASE_URL = "https://async-race-api-production-82d0.up.railway.app"
+
 export const getCars = async (page: number, limit: number) => {
-  const res = await fetch(`http://localhost:3000/garage/?_limit=${limit}&_page=${page}`, {
+  const res = await fetch(`${BASE_URL}/garage/?_limit=${limit}&_page=${page}`, {
     method: "get",
   }).then((res) => res.json());
   return res;
 };
 export const getCarsCount = async () => {
-  const res = await fetch(`http://localhost:3000/garage/`, { method: "get" });
+  const res = await fetch(`${BASE_URL}/garage/`, { method: "get" });
   return res;
 };
 
 export const getCar = async (id: number) => {
-  const res = await fetch(`http://localhost:3000/garage/${id}`, { method: "get" }).then((res) => res.json());
+  const res = await fetch(`${BASE_URL}/garage/${id}`, { method: "get" }).then((res) => res.json());
   return res;
 };
 
 export const removeCar = async (id: number) => {
-  await fetch(`http://localhost:3000/garage/${id}`, { method: "delete" });
+  await fetch(`${BASE_URL}/garage/${id}`, { method: "delete" });
   const winner = await getWinner(id)
   if(Object.keys(winner).length != 0){
     deleteWinner(id);
@@ -23,7 +25,7 @@ export const removeCar = async (id: number) => {
 };
 
 export const createCar = async (name: string, color: string) => {
-  const res = await fetch(`http://localhost:3000/garage`, {
+  const res = await fetch(`${BASE_URL}/garage`, {
     method: "post",
     headers: {
       "Content-Type": "application/json",
@@ -34,7 +36,7 @@ export const createCar = async (name: string, color: string) => {
 };
 
 export const updateCar = async (id: number, name: string, color: string) => {
-  const res = await fetch(`http://localhost:3000/garage/${id}`, {
+  const res = await fetch(`${BASE_URL}/garage/${id}`, {
     method: "put",
     headers: {
       "Content-Type": "application/json",
@@ -46,7 +48,7 @@ export const updateCar = async (id: number, name: string, color: string) => {
 
 export const toggleEngine = async (id: number, status: string) => {
   const queryParams = new URLSearchParams({ id: `${id}`, status });
-  const res = await fetch(`http://localhost:3000/engine/?${queryParams}`, {
+  const res = await fetch(`${BASE_URL}/engine/?${queryParams}`, {
     method: "PATCH",
   });
   return res.json();
@@ -54,7 +56,7 @@ export const toggleEngine = async (id: number, status: string) => {
 
 export const createWinner = async (id: number, wins: number, time: number) => {
   try {
-    const res = await fetch(`http://localhost:3000/winners/`, {
+    const res = await fetch(`${BASE_URL}/winners/`, {
       method: "post",
       headers: {
         "Content-Type": "application/json",
@@ -73,7 +75,7 @@ export const createWinner = async (id: number, wins: number, time: number) => {
 };
 
 export const getWinner = async (id: number) => {
-  const res = await fetch(`http://localhost:3000/winners/${id}`, {
+  const res = await fetch(`${BASE_URL}/winners/${id}`, {
     method: "get",
   }).then(res => res.json());
   return res;
@@ -81,7 +83,7 @@ export const getWinner = async (id: number) => {
 
 export const updateWinner = async (id: number, wins: number, time: number) => {
   try {
-    const res = await fetch(`http://localhost:3000/winners/${id}`, {
+    const res = await fetch(`${BASE_URL}/winners/${id}`, {
       method: "put",
       headers: {
         "Content-Type": "application/json",
@@ -99,21 +101,21 @@ export const updateWinner = async (id: number, wins: number, time: number) => {
 };
 
 export const deleteWinner = async (id: number) => {
-  const res = await fetch(`http://localhost:3000/winners/${id}`, {
+  const res = await fetch(`${BASE_URL}/winners/${id}`, {
     method: "delete",
   });
   return res;
 };
 
 export const getWinners = async (page: number, limit: number, sort: string, order: string) => {
-  const res = await fetch(`http://localhost:3000/winners/?_limit=${limit}&_page=${page}&_sort=${sort}&_order=${order}`, { method: "get" }).then(
+  const res = await fetch(`${BASE_URL}/winners/?_limit=${limit}&_page=${page}&_sort=${sort}&_order=${order}`, { method: "get" }).then(
     (res) => res.json()
   );
   return res;
 };
 
 export const getTotalWinners = async () => {
-  const res = await fetch(`http://localhost:3000/winners`, { method: "get" }).then(
+  const res = await fetch(`${BASE_URL}/winners`, { method: "get" }).then(
     (res) => res.json()
   );
   return res.length;
